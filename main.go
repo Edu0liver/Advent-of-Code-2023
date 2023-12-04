d0bc9dda44ec7fdd7920b2b2c6c4b8737606e565
Eduardo <eduardooliveira256@gmail.com>
2023-12-04 00:35:20 -0300

    test inputs

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/Edu0liver/Advent-of-Code-2023/day3"
+	"github.com/Edu0liver/Advent-of-Code-2023/day1"
+	"github.com/Edu0liver/Advent-of-Code-2023/day2"
 )
 
 func main() {
-	// fmt.Println("Day 1:", *day1.Exec())
-	// fmt.Println("Day 2:", *day2.Exec())
-	fmt.Println("Day 3:", *day3.Exec())
+	fmt.Println("Day 1:", *day1.Exec())
+	fmt.Println("Day 2:", *day2.Exec())
+	// fmt.Println("Day 3:", *day3.Exec())
 }
